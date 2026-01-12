1258d2bbd8989b5ea6ce90a256bc58c8fb4b5edb
oreoluwanimi1211 <oladimejioreoluwa070@gmail.com>
2026-01-12 22:08:00 +0100

    first commit

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,4 +6,14 @@ class UserRegistrationForm(forms.ModelForm):
     confirm_password = forms.CharField(widget=forms.PasswordInput())
     class Meta:
         model = User
-        fields = ['first_name','last_name','username', 'email','phone_no','role', 'password']
+        fields = ['first_name','last_name','username', 'email','role', 'password']
+
+    def clean(self):
+        clean_data = super(UserRegistrationForm, self).clean()
+        password = clean_data.get('password')
+        confirm_password = clean_data.get('confirm_password')
+
+        if password != confirm_password:
+            raise forms.ValidationError(
+                 'password does not match'
+                )
